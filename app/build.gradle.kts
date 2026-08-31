a440de8488a25acc20333050868a01d188129e7e
prodipsarkar5911-droid <prodipsarkar5911@gmail.com>
2026-08-31 08:03:24 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     alias(libs.plugins.google.devtools.ksp)
     alias(libs.plugins.roborazzi)
     alias(libs.plugins.secrets)
-    alias(libs.plugins.google.services)
+    // alias(libs.plugins.google.services) // Disabled to prevent missing google-services.json build failure
 }
 
 android {
